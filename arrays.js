34554b952357cf90eede5358198ec47b8df6725a
Ben Rosner <br305893@gmail.com>
2018-08-27 00:27:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,6 +9,7 @@ function addElementToBeginningOfArray(array, element){
   array = [element, ...array]
   return array
 }
+
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
@@ -18,10 +19,16 @@ function addElementToEndOfArray(array, element){
   array = [...array, element]
   return array
 }
+
 function destructivelyAddElementToEndOfArray(array, element){
   array.push(element)
   return array
 }
+
 function accessElementInArray(array, index){
  return (array[index])
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
 }
