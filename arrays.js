6dc101465c8b18df3a3faeca3784082a886100c1
Ben Rosner <br305893@gmail.com>
2018-08-27 00:54:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -45,3 +45,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 function removeElementFromEndOfArray(array){
   array.slice(0, array.length - 1)
 }
+
+function removeElementFromEndOfArray(array){
+  
+}
